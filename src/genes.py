a9a92ffa1a6d659d4a6475fc71e365d0ff1c3f32
Felipe Vaz Peres <felipe.vzps@gmail.com>
2021-01-18 05:59:08 -0300

    Update genes.py
    Utilizei o módulo SeqIO do biopython para facilitar o trabalho com o arquivo fasta. Desta maneira, é possível reutilizar este script e trabalhar com arquivos contendo milhares de sequências. 
    OBS1: O SeqIO.parse divide o arquivo em features (classes de identificadores, sequências, etc), o que facilita trabalhar com arquivos gigantes sem precisar definir uma variável para cada sequência. 
    OBS2: O SeqIO.write permite escrever o output sem armazenar as sequências e identificadores na memória, melhorando a eficiência do algoritmo, ao contrário do armazenamento das sequências em uma lista, por exemplo, que exige que todo o conteúdo da lista seja armazenado e lido na memória antes da escrita do output.

--- src/genes.py
+++ src/genes.py
@@ -1,5 +1,8 @@
 import os
+from Bio import SeqIO
 
+# Requisios = Biopython
+#!pip install biopython
 
 # Input
 # ---aaattcc---cccc--
@@ -23,66 +26,24 @@ def contar_no_final(sequencia):
 
     return contar_no_comeco(reversed(sequencia))
 
-def editar_sequencias(seq1, seq2):
-    _inicio = contar_no_comeco(seq1)
-    _final = contar_no_final(seq1)
+def editar_sequencias(seq):
+    _inicio = contar_no_comeco(seq)
+    _final = contar_no_final(seq)
     # se o final for 0, precisamos ajustar
     
     # Sim, podemos retornar duas coisas em Python
-    return (seq1[_inicio:-_final if _final != 0 else len(seq1)], 
-            seq2[_inicio:-_final if _final != 0 else len(seq2)])
-
-def teste():
-    '''Este é o teste'''
-
-    seq1 = '---aaattcc---cccc--'
-    seq2 = 'aactgtgactgcatgcatgactgactg'
-
-    ed1, ed2 = editar_sequencias(seq1, seq2)
-    print(ed1)
-    print(ed2)
-
-def encontrar_gb(linhas):
-    '''Econtra o índice onda há ">gb" '''
-
-    indice = 0
-    for l in linhas:
-        if '>gb' in l:
-            break
-        indice = indice + 1
-    return indice
+    return seq[_inicio:-_final if _final != 0 else len(seq)
 
 if __name__ == "__main__":
-    # abre o arquivo para leitura
-    teste()
-    with open('input/DENV1-X-gb_A75711.fasta.aln') as fasta:
-        conteudo = fasta.read()
-        # dividimos o conteúdo em uma lista nas quebra de linhas 
-        # equivalente ao readlines()
-        linhas = conteudo.split('\n')
-        # print(linhas)
-        cabecalho1 = linhas[0]
-        indice = encontrar_gb(linhas)
 
-        cabecalho2 = linhas[indice]
-        # reconstruímos as sequências a partir da lista
-        sequencia1 = ''.join(linhas[1:indice])
-        sequencia2 = ''.join(linhas[indice+1:])
+  # Abrindo o arquivo fasta e lendo seu conteúdo com o módulo SeqIO
+  # O SeqIO é um módulo do biopython que facilita o trabalho com arquivos de texto utilizados na bioinformática, como fasta, fastq, genbank, etc.
+  # O SeqIO.parse nos permite identificar diversas features destes arquivos, por exemplo: identificador da sequência = record.id, sequência = record.seq
+  with open("/content/teste/input.fasta", "r") as handle, open("output.fasta", "w") as output_handle:
+    for record in SeqIO.parse(handle, "fasta"):
 
-        # abre um novo arquivo para escrita
-        with open(os.path.join('output', 'resultado.fasta'), 'w') as res:
-            # recebemos as duas sequências editadas retornadas
-            editada1, editada2 = editar_sequencias(sequencia1, sequencia2)
-            # escreve sequencia editada
-            res.write(cabecalho1 + '\n')
-            res.write(editada1 + '\n')
-            res.write(cabecalho2 + '\n')
-            res.write(editada2)
-    
-    
-    
+      # Encontrando cada sequência do arquivo e editando-as com a função "editar_sequencias"
+      record.seq = editar_sequencias(record.seq)
 
-    
-            
-    
-
+      # Escrevendo cada uma das sequências editadas em um output no formato fasta
+      SeqIO.write(record, output_handle, "fasta")
